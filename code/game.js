ada00b06b5a3b753ebed92660765e4f93ec8f660
Alex Rodriguez <alex-kampe@Alexs-MacBook-Air.local>
2025-05-15 15:57:32 +0200

    Gaps on screen removed

--- code/game.js
+++ code/game.js
@@ -1,8 +1,9 @@
 const config = {
   type: Phaser.AUTO,
-  width: window.innerWidth,
-  height: window.innerHeight,
+  width: '100%',
+  height: '100%',
   backgroundColor: '#0061b5',
+  parent: 'game',
   scale: {
     mode: Phaser.Scale.FIT,
     autoCenter: Phaser.Scale.CENTER_BOTH,
